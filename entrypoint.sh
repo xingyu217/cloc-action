80691d93d5517794fdbf345d271ed3c94b4fcebf
changenameYanjingzhu <yanjingzhu123@outlook.com>
2020-03-24 14:44:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 cloc $(git rev-parse HEAD) | tee -a cloc.txt
 cat cloc.txt
-line=$( tail -n 2 cloc.txt )
+line=$( tail -2 cloc.txt | head -1 )
 echo "::set-output name=lines::$line"
