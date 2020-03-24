58b6cacd199c52b999ad09b81280d6dd7525e48f
changenameYanjingzhu <yanjingzhu123@outlook.com>
2020-03-24 16:11:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 cloc $(git rev-parse HEAD) | tee -a cloc.txt
 line=$( tail -n 2 cloc.txt | head -1 )
 echo $line
-#IFS=' ' read -r -a arr <<< "$(echo $line)"
-arr=$( echo $line | tr " " "\n" )
+IFS=' ' read -r -a arr <<< "${line}"
+#arr=$( echo $line | tr " " "\n" )
 for i in "${arr[@]}"
 do
     echo $i
