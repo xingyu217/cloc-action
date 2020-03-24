073d57c1d3b567c477cc24d2efddc1c90ae773b1
changenameYanjingzhu <yanjingzhu123@outlook.com>
2020-03-24 15:08:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 cloc $(git rev-parse HEAD) | tee -a cloc.txt
 line=$( tail -n 2 cloc.txt | head -1 )
-line_arr=($line)
-echo $line_arr[0]
-echo $line_arr[1]
-echo $line_arr[4]
+arr=$(echo $line | tr " " "\n")
+
+for x in $arr
+do
+    echo "\"$x\""
+done
+
 echo "::set-output name=lines::$line_arr[4]"
